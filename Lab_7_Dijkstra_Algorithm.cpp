2be13b7b958d2d07dc8ef9ca847c06dfad5e5721
Yug Shah <yugshah197@gmail.com>
2026-08-01 12:53:37 +0530

    ai-upgrade: improve Lab_7_Dijkstra_Algorithm.cpp

--- Lab_7_Dijkstra_Algorithm.cpp
+++ Lab_7_Dijkstra_Algorithm.cpp
@@ -1,24 +1,27 @@
-#include <stdio.h>
+#include <cstdio>
 #define INF 999
+#define MAX 20
 
-int dist[20][20], n, e;
+int dist[MAX][MAX];
+int n, e;
 
 void dijkstra(int src)
 {
-    int output[20], vis[20] = {0};
-    int i, j, u;
+    int output[MAX];
+    int vis[MAX] = {0};
 
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         output[i] = INF;
     }
     output[src] = 0;
 
-    for (i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         int min = INF;
+        int u = -1;
 
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             if (!vis[j] && output[j] < min)
             {
@@ -27,9 +30,14 @@ void dijkstra(int src)
             }
         }
 
+        if (u == -1)
+        {
+            break;
+        }
+
         vis[u] = 1;
 
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             if (!vis[j] && dist[u][j] != INF && output[u] + dist[u][j] < output[j])
             {
@@ -40,7 +48,7 @@ void dijkstra(int src)
 
     printf("\nShortest paths from Source Node %d:\n", src);
     printf("Vertex\tDistance\n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (output[i] == INF)
             printf("%d \t INF\n", i);
@@ -51,49 +59,42 @@ void dijkstra(int src)
 
 int main()
 {
-    int u, v, w, i, j, src;
+    int u, v, w, src;
 
     printf("Enter vertices and edges: ");
-    scanf("%d %d", &n, &e);
-
-    for (i = 0; i < n; i++)
+    if (scanf("%d %d", &n, &e) != 2 || n <= 0 || n > MAX || e < 0)
     {
-        for (j = 0; j < n; j++)
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
         {
-            if (i == j)
-                dist[i][j] = 0;
-            else
-                dist[i][j] = INF;
+            dist[i][j] = (i == j) ? 0 : INF;
         }
     }
 
     printf("Enter edges (u v w):\n");
-    for (i = 0; i < e; i++)
+    for (int i = 0; i < e; i++)
     {
-        scanf("%d %d %d", &u, &v, &w);
+        if (scanf("%d %d %d", &u, &v, &w) != 3 || u < 0 || u >= n || v < 0 || v >= n)
+        {
+            printf("Invalid edge.\n");
+            return 1;
+        }
         dist[u][v] = w;
     }
 
     printf("Enter source vertex: ");
-    scanf("%d", &src);
+    if (scanf("%d", &src) != 1 || src < 0 || src >= n)
+    {
+        printf("Invalid source vertex.\n");
+        return 1;
+    }
 
     dijkstra(src);
 
     return 0;
 }
-
-// Enter vertices and edges: 4 5  
-// Enter edges (u v w): 
-// 0 1 4                                                                      
-// 0 2 1                     
-// 2 1 2                                                                      
-// 1 3 1                     
-// 2 3 5
-// Enter source vertex: 0
-
-// Shortest paths from Source Node 0:
-// Vertex  Distance
-// 0        0
-// 1        3
-// 2        1
-// 3        4
